d46da258ce4e49427f53881a53c1e4bb1d5577ff
Dandelionym <yingmingpu@gmail.com>
2023-11-13 14:18:14 +0800

    reformat publication

---
+++ run_publication.py
@@ -0,0 +1,33 @@
+import numpy
+import pandas as pd
+
+df = pd.read_excel("./nano_pubs.xlsx")
+
+
+all_pubs = ""
+
+for idx, i in enumerate(df.iterrows()):
+
+    if numpy.isnan(df['序号'][idx]):
+        continue
+
+    sample = f"""
+    
+        <div class="item_line">
+            <div class='left_box'><span>{int(df['序号'][idx])}. </span><span class='pub_title'>{str(df['论文标题'][idx])}</span>,
+                <span class='pub_author'>{str(df['作者'][idx]).replace("Hongyu Chen", "<b>Hongyu Chen</b>")}</span>,
+                <span class='pub_publish'><i><u>{str(df['期刊'][idx])}</u></i></span>, <span
+                        class='pub_year'>{int(df['年份'][idx])}</span>, <span class='pub_page'>{str(df['页码'][idx]).replace(" ", "")}</span>.<a href='{
+    str(df['DOI'][idx]).strip().replace(" ", "") if "https://doi.org/" in str(df['DOI'][idx]).strip() else "https://doi.org/" + str(df['DOI'][idx]).strip().replace(" ", "")
+    }'><span class='pub_doi'>link</span></a></div>
+            <div class="img_box"><img class="img-thumbnail img-rounded face_img" src="images/{int(df['序号'][idx])}.bmp"></div>
+            <br/><br/><br/>
+        </div>
+        
+    """
+    print(sample)
+    all_pubs += sample
+
+
+with open("./pubs.html", mode='w', encoding='utf-8') as f:
+    f.write(all_pubs)
